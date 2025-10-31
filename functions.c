74b518aa5a5b99fa7596f20491e3d4ddb7923648
Lucas Amorim <luc4ss00a@gmail.com>
2025-10-31 10:37:03 -0300

    [v4.2] File encryption feature added

--- functions.c
+++ functions.c
@@ -68,7 +68,6 @@ int intermediate_process(int msgLength, char ** preparedMsg, char * message){
 	for (int i = 1; i < msgLength; i++){
 		arr[i] = strtol(strtok(NULL, "\\x"), &endptr, 16);
 	}
-	
 	// Decryption
 	char * arrChar = calloc(msgLength + 1, sizeof(char));
 	if (arrChar == NULL){
@@ -78,25 +77,55 @@ int intermediate_process(int msgLength, char ** preparedMsg, char * message){
 	for (int i = 0; i < msgLength; i++){
 		arrChar[i] = (char)arr[i];
 	}
-	
 	// Cleanup
 	free(arr);
-
 	// Result
 	*preparedMsg = arrChar;
 	return 0;
 }
 
+int read_file(char * filename, char ** msgOut){
+	FILE *PTR_file;
+	char buf[1024] = {0};
+	unsigned int bytesRead = 0;
+	PTR_file = fopen(filename, "rb");
+	if (PTR_file == NULL){
+		perror("[FOPEN_ERR]");
+		return -1;
+	}
+	bytesRead = fread(buf, 1, (sizeof(buf) -1), PTR_file);
+	*msgOut = calloc(1, (bytesRead + 1));
+	printf("[+] %d bytes read.\n", bytesRead);
+	//*msgOut = buf;
+	strncpy(*msgOut, buf, bytesRead);
+	fclose(PTR_file);
+	return 0;
+}
+
 void print_usage(char * argv[]){
-        printf("XOR_Crypter - Stable v3.3\n");
+        printf("XOR_Crypter - Stable v4.2\n");
         printf("%s [-h], [--help]: Prints this.\n", argv[0]);
-        printf("%s [-k <KEY>] [-m <MESSAGE>]: Encrypts message using key.\n", argv[0]);
-        printf("%s [-k <KEY>] [-m <HEX>] [-d]: Decrypts message using key.\n", argv[0]);
-        printf("%s [--debug] enables debug output (key/message size information).\n", argv[0]);
+	printf("%s [-m <MESSAGE>]: Reads message from stdin.\n", argv[0]);
+	printf("%s [-M <FILE>]:    Reads message from file.\n", argv[0]);
+	printf("%s [-k <KEY>]:     Reads message from stdin\n", argv[0]);
+	printf("%s [-K <FILE>]:    Reads key from file.\n", argv[0]);
+        printf("%s [-d DECRYPT]:   Enables decrypt mode.\n", argv[0]);
+	printf("%s [--debug]       Enables debug output (key/message size information).\n", argv[0]);
+	printf("[INFO]: Try %s --examples.\n", argv[0]);
+}
+
+void print_examples(char * argv[]){
+	printf("XOR_Crypter - Stable v4.2\n");
+        printf("%s [-e], [--examples]: Prints this.\n", argv[0]);
+        printf("%s [-k <KEY>]  [-m <MESSAGE>]:  Encrypts message using key.\n", argv[0]);
+        printf("%s [-k <KEY>]  [-m <HEX>] [-d]: Decrypts message using key.\n", argv[0]);
+	printf("%s [-k <KEY>]  [-M <FILE>]:     Encrypts the file and show output on stdout.\n", argv[0]);
+	printf("%s [-K <FILE>] [-M <FILE>]:     Import key and file from different files.\n", argv[0]);
 }
 
 void print_encrypted(char * message, int size){
-        for (int i = 0; i < size; i++){
+        printf("[+] ");
+	for (int i = 0; i < size; i++){
                 printf("\\x%02x",message[i]);
         }
         printf("\n");
--- main.c
+++ main.c
@@ -7,17 +7,21 @@
 
 int main(int argc, char * argv[]){
 	int c = 0;
+	int readFile = 0;
 	char * key = NULL;
+	char * keyFile = NULL;
 	char * msg = NULL;
+	char * msgFile = NULL;
 	bool isDebug = false;
 	bool toDecrypt = false;
 
 	static struct option long_options[] = {
-    	{ "debug", 0, NULL, 'a' },
-    	{ "help", 0, NULL, 'h' },
+    	{ "debug"    , 0, NULL, 'a' },
+    	{ "help"     , 0, NULL, 'h' },
+	{ "examples" , 0, NULL, 'e' },
 	{ 0, 0, 0, 0}};
 
-	while ((c = getopt_long(argc, argv, "k:m:dh", long_options, NULL)) != -1){
+	while ((c = getopt_long(argc, argv, "k:m:dhM:K:", long_options, NULL)) != -1){
 		switch (c){
 			case 'h':
 				print_usage(argv);
@@ -25,8 +29,14 @@ int main(int argc, char * argv[]){
 			case 'k':
 				key = optarg;
 				break;
+			case 'K':
+				keyFile = optarg;
+				break;
 			case 'm':
 				msg = optarg;
+				break;
+			case 'M':
+				msgFile = optarg;
 				break;
 			case 'd':
 				toDecrypt = true;
@@ -34,6 +44,9 @@ int main(int argc, char * argv[]){
 			case 'a':
 				isDebug = true;
 				break;
+			case 'e':
+				print_examples(argv);
+				return 0;
 			case '?':
 				break;
 			
@@ -42,7 +55,24 @@ int main(int argc, char * argv[]){
 		}
 	}
 
-	if (key && msg){
+	if ((msg && msgFile) || (key && keyFile)){
+		printf("[ERR]: You must select either text or file, but not both.\n");
+		printf("[INFO]: Try %s --help.\n", argv[0]);
+		return -1;
+	}
+	if (keyFile){
+		readFile = read_file(keyFile, &key);
+		/* FILE:
+		 * - [X] Abrir arquivo binário
+		 * - [X] Ler conteúdo do arquivo
+		 * - [ ] Outputar conteúdo pós XOR em um arquivo
+		 * 	- [ ] O nome do arquivo deve ser aleatório
+		 */	 
+	}
+	if (msgFile){
+		readFile = read_file(msgFile, &msg);
+	}
+	if (msg && key){
 		int msgLength = 0;
 		int keyLength = 0;
 		if (get_length(msg, key, &msgLength, &keyLength, toDecrypt, isDebug) == -1){
@@ -60,16 +90,23 @@ int main(int argc, char * argv[]){
 			char * decryptedMsg = NULL;
 			intermediate_process(msgLength, &preparedMsg, msg);
 			decryptedMsg = XOR(preparedMsg, key, msgLength, keyLength);
-			printf("%s\n", decryptedMsg);
+			printf("[+] %s\n", decryptedMsg);
 			
 			// Cleanup 
 			free(preparedMsg);
 			free(decryptedMsg);
 		}
 	} else {
-		printf("Key and message are required arguments.\n");
-		printf("Try %s --help.\n", argv[0]);
+		printf("[ERR]: Key and message are required arguments.\n");
+		printf("[INFO]: Try %s --help.\n", argv[0]);
+		return -1;
 	}
-
+	// Cleanup
+	if (keyFile){
+		free(key);
+	}
+	if (msgFile){
+		free(msg);
+	}
 	return EXIT_SUCCESS;
 }
